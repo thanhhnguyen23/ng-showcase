1801b849666c20f377845b2bc70f66787c4ec90e
Thanh Nguyen <thanh.h.nguyen23@gmail.com>
2022-12-26 23:04:07 -0500

    add super heroes component

---
+++ src/app/toh/superheroes/superheroes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-superheroes',
+  templateUrl: './superheroes.component.html',
+  styleUrls: ['./superheroes.component.css']
+})
+export class SuperheroesComponent implements OnInit {
+
+  constructor() { }
+  
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+}
